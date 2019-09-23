c27746181079dc9bef4097fc25ed9a331f5aae29
generous-vector-9581 <ojappah@gmail.com>
2019-09-23 22:09:39 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each(array) # put argument(s) here
-  # code here
+words = ['hi', 'hello', 'bye', 'goodbye']
+def my_each(words) 
+  if block_given?
+  i = 0
+  while i < words.length
+    yield words[i]
+    i = i + 1
+  end 
+  words
+else 
+  puts "Hey! No block was given!"
+end
 end
